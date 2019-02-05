cd72776ca81a3d0eb301953b4ecb3e683a364e89
Alex Clements <alexfjclements@gmail.com>
2019-02-05 14:07:23 +0000

    pushing to react branch for merge with develop

--- demo/mythical_creatures_zoo/src/components/NavBar/RegionsDropDown.js
+++ demo/mythical_creatures_zoo/src/components/NavBar/RegionsDropDown.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './NavBar.css';
 
 // class RegionsDropDown extends Component{
@@ -9,21 +9,21 @@ import './NavBar.css';
 //    }
 // }
 
-const RegionsDropDown = (props, {creatures = []}) => {
-   const options = creatures.map((creature, index) => {
-     return <option value={index} key={index}>{creature.origin_location}</option>
-   })
- 
-   function handleChange(event) {
-     props.onRegionSelected(event.target.value);
-   }
- 
-   return (
-     <select id="region-selector" onChange={handleChange} defaultValue="default">
-       <option disabled value="default">Choose a Region...</option>
-       { options }
-     </select>
-   )
- }
+const RegionsDropDown = (props, { creatures = [] }) => {
+  const options = creatures.map((creature, index) => {
+    return <option value={index} key={index}>{creature.origin_location}</option>
+  })
+
+  function handleChange(event) {
+    props.onRegionSelected(event.target.value);
+  }
+
+  return (
+    <select id="region-selector" onChange={handleChange} defaultValue="default">
+      <option disabled value="default">Choose a Region...</option>
+      {options}
+    </select>
+  )
+}
 
 export default RegionsDropDown;
