b284d55fa4aa474975607208d0164f90fed86b0b
Jake Thwaites <jakethwaites@hotmail.co.uk>
2019-02-06 10:35:36 +0000

    started addCreature link

--- demo/mythical_creatures_zoo/src/components/Mains/CreatureList.js
+++ demo/mythical_creatures_zoo/src/components/Mains/CreatureList.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import CreatureDetail from './CreatureDetail.js';
 import './Main.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddCreature from './AddCreature.js';
 
 const CreatureList = ({ creatures = [] }) => {
 
    const listCreatures = creatures.map((creature, index) => {
       return (
-         <li key={index} className="creature_list">
+          <li key={index} className="creature_list">
             <CreatureDetail
                name={creature.name}
                breed={creature.breed}
@@ -16,11 +18,13 @@ const CreatureList = ({ creatures = [] }) => {
          </li>
       );
    });
-
    return (
+     <Fragment>
+      <Route path="/AddCreature" component={AddCreature}></Route>
       <ul>
          {listCreatures}
       </ul>
+      </Fragment>
    );
 
 }
