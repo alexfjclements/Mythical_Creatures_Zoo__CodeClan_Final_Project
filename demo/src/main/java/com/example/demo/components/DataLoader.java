745b558149565ee3f4829121d029bcca6a45fea1
Ray <ray.greenan.89@outlook.com>
2019-02-06 11:42:07 +0000

    Added stuff to data loader

--- demo/src/main/java/com/example/demo/components/DataLoader.java
+++ demo/src/main/java/com/example/demo/components/DataLoader.java
@@ -29,24 +29,51 @@ public class DataLoader implements ApplicationRunner {
     }
 
     public void run(ApplicationArguments args){
+
+        //MENAGERIE
+        Menagerie mythicalMenagerie = new Menagerie("Mythical Menagerie", 10);
+        menagerieRepository.save(mythicalMenagerie);
+
+        //CREATURES GREEK
        MythicalCreature harpy1 = new MythicalCreature("Jarpy", Gender.Male, "IKillEat", "Greek", "", "Harpy");
        mythicalCreaturesRepository.save(harpy1);
        MythicalCreature harpy2 = new MythicalCreature("Flarpy", Gender.Female, "IKillEat", "Greek", "", "Harpy");
        mythicalCreaturesRepository.save(harpy2);
-       Enclosure enclosure = new Enclosure(2, "Harpy Enclosure", "Greek");
-       enclosureRepository.save(enclosure);
+
+       MythicalCreature unicorn1 = new MythicalCreature("Melvin", Gender.Male, "IKillEat", "Greek", "", "Unicorn");
+       mythicalCreaturesRepository.save(unicorn1);
+       MythicalCreature unicorn2 = new MythicalCreature("Melvinetta", Gender.Female, "IKillEat", "Greek", "", "Unicorn");
+       mythicalCreaturesRepository.save(unicorn2);
+
+       //CREATURES NORSE
+        MythicalCreature wolf1 = new MythicalCreature("Geri", Gender.NonBinary, "IKillEat", "Norse", "", "Wolf");
+        mythicalCreaturesRepository.save(wolf1);
+        MythicalCreature wolf2 = new MythicalCreature("Freki", Gender.NonBinary, "IKillEat", "Norse", "", "Wolf");
+        mythicalCreaturesRepository.save(wolf2);
+
+        MythicalCreature horse1 = new MythicalCreature("Gná", Gender.NonBinary, "IKillEat", "Norse", "", "Horse");
+        mythicalCreaturesRepository.save(horse1);
+        MythicalCreature horse2 = new MythicalCreature("Hófvarpnir", Gender.NonBinary, "IKillEat", "Norse", "", "Horse");
+        mythicalCreaturesRepository.save(horse2);
+
+       //ENCLOSURES
+       Enclosure harpyEnclosure = new Enclosure(2, "Harpy Enclosure", "Greek"); enclosureRepository.save(harpyEnclosure);
+       Enclosure unicornEnclosure = new Enclosure(2, "Scotland","Greek"); enclosureRepository.save(unicornEnclosure);
+       Enclosure wolfEnclosure = new Enclosure(2, "Norse Wolves", "Norse"); enclosureRepository.save(wolfEnclosure);
+       Enclosure horseEnclosure = new Enclosure(2, "Norses", "Norse"); enclosureRepository.save(horseEnclosure);
+
+        //VISITORS
+        Visitor visitor1 = new Visitor(); visitorRepository.save(visitor1);
+        Visitor visitor2 = new Visitor(); visitorRepository.save(visitor2);
+        Visitor visitor3 = new Visitor(); visitorRepository.save(visitor3);
+        Visitor visitor4 = new Visitor(); visitorRepository.save(visitor4);
+        Visitor visitor5 = new Visitor(); visitorRepository.save(visitor5);
+        Visitor visitor6 = new Visitor(); visitorRepository.save(visitor6);
+        Visitor visitor7 = new Visitor(); visitorRepository.save(visitor7);
+        Visitor visitor8 = new Visitor(); visitorRepository.save(visitor8);
+        Visitor visitor9 = new Visitor(); visitorRepository.save(visitor9);
+        Visitor visitor10 = new Visitor(); visitorRepository.save(visitor10);
 
 
-
-
-
-
-       MythicalCreature unicorn = new MythicalCreature("Melvin", Gender.Female, "IDontKill", "Greek", "", "Unicorn");
-       mythicalCreaturesRepository.save(unicorn);
-
-       Menagerie menagerie = new Menagerie("Name", 10);
-       menagerieRepository.save(menagerie);
-       Visitor visitor1 = new Visitor();
-       visitorRepository.save(visitor1);
     }
 }
