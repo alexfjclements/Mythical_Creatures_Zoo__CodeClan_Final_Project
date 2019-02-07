0032afa8cc3d728035ebd3215c317a0abf944a3f
KevMcEwan <kev_mcewan@hotmail.com>
2019-02-07 11:28:16 +0000

    more styling

--- demo/mythical_creatures_zoo/src/components/Mains/CreatureForm.js
+++ demo/mythical_creatures_zoo/src/components/Mains/CreatureForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Main.css';
+
 
 
 
@@ -22,28 +22,36 @@ const CreatureForm = (props) => {
 
     return (
     <div className="add-creature-form">
-      <h1>Add New Creature</h1>
+      <h2>Add New Creature</h2>
       <form onSubmit={handleSubmit}>
         <label>Name</label>
         <input type="text" id="name" name="name" />
 
+        <br></br>
+        <br></br>
         <label>Gender</label>
+        <div className="gender_input">
         <input type="radio" value="male" name="gender" /> Male <br />
         <input type="radio" value="female" name="gender" /> Female <br />
         <input type="radio" value="Non Binary" name="gender" /> Non Binary <br />
+        </div>
 
+       
         <label for="killBehaviour">Does it kill humans?</label>
         <select name="killBehaviour">
           <option value="IKillEat">Yes, it eats humans!</option>
           <option value="IDontKill">No way, Jose!</option>
         </select>
 
+        
         <label>Origin Location</label>
         <input type="text" id="origin-location" name="originLocation" />
 
+        
         <label>Description</label>
         <textarea name="description" for="add-creature-form">Add Description Here</textarea>
 
+       
         <label>Breed</label>
         <input type="text" id="breed" name="breed" />
 
